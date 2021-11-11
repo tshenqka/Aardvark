32831de7aae221a13e2e5d34b30bb27ea4c8f7f8
jaysee <13825-jaysee@users.noreply.git.uwaterloo.ca>
2021-11-11 11:36:57 -0500

    Changes to wifi printing

--- wifiboard/wifiboard.ino
+++ wifiboard/wifiboard.ino
@@ -1,107 +1,119 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
 
-//#include "arduino_secrets.h"
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = "eduroam";  // your WPA2 enterprise network SSID (name)
 char user[] = "c54chung@uwaterloo.ca";  // your WPA2 enterprise username
 char pass[] = "";  // your WPA2 enterprise password
 int status = WL_IDLE_STATUS;     // the WiFi radio's status
 
+const char server[] = "encyz7exee95.x.pipedream.net";
+
 WiFiClient client;
 
-void setup() {
-  //Initialize serial and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+#define VERBOSE_WIFI false
+void wifiprint(char *msg) {
+  if (VERBOSE_WIFI) {
+    Serial.print(msg);
   }
+}
 
-  // check for the WiFi module:
+void wifiprint(char *msg) {
+  if (VERBOSE_WIFI) {
+    Serial.print(msg);
+    Serial.println();
+  }
+}
+
+void setup() {
+  // Initialize serial and wait for port to open
+  Serial.begin(9600);
+  while (!Serial); 
+
+  Serial.println("Begin startup");
+
+  // Abort if there is no wifi module
   if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    // don't continue
+    Serial.println("Communication with WiFi module failed! Aborting...");
     while (true);
   }
 
+  // Check firmware
   String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) 
+    Serial.println("Firmware upgrade found.");
 
-  // attempt to connect to WiFi network:
+  Serial.println("Setup complete");
+}
+
+void connect() {
+  // Attempt to connect to eduroam
   while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.print("Attempting to connect to network with SSID: ");
     Serial.println(ssid);
-    // Connect to WPA2 enterprise network:
-    // - You can optionally provide additional identity and CA cert (string) parameters if your network requires them:
-    //      WiFi.beginEnterprise(ssid, user, pass, identity, caCert)
+    // status = WiFi.beginEnterprise(ssid, user, pass, identity, caCert)
     status = WiFi.beginEnterprise(ssid, user, pass);
 
-    // wait 10 seconds for connection:
-    delay(10000);
+    // Check the connection every 5 seconds
+    delay(5000);
+    if (status != WL_CONNECTED) {
+      Serial.println("Connection failed. Retrying in 3 seconds.");
+      delay(3000);
+    }
   }
 
-  // you're connected now, so print out the data:
-  Serial.print("You're connected to the network");
-  printCurrentNet();
-  printWifiData();
-
-}
-
-void loop() {
-  // check the network connection once every 10 seconds:
-  delay(5000);
-  printCurrentNet();
-  WiFiClient client;
-  Serial.println("attempting connection to bucket");
-
- if (client.connect("encyz7exee95.x.pipedream.net", 80)) {
-      Serial.println("connected");
-      // Make a HTTP request:
-      client.println("GET / HTTP/1.0");
-      client.println("Host: encyz7exee95.x.pipedream.net");
-      client.println("Accept: */*");
-      client.println("Message: Hello, from the arduino!");
-      client.println("Connection: close");
-      client.println();
-    }
-}
-
-void printWifiData() {
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print your MAC address:
+  Serial.println("Connection successful.");
   byte mac[6];
   WiFi.macAddress(mac);
   Serial.print("MAC address: ");
   printMacAddress(mac);
+  Serial.print("Local IP Address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Signal strength (RSSI): ");
+  Serial.println(WiFi.RSSI());
 }
 
-void printCurrentNet() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+void internetCheck() {
+  // Check the network connection once every 5 seconds. Returns when connection to the server is lost
+  while (true) {
+    delay(5000);
+    bool connected = false;
+    for (int i = 0; i < 3; i++) {
+      if (client.connect(server, 80)) {
+        Serial.print("Connection established to ");
+        Serial.println(server);
+        // Make a HTTP request:
+        client.println("GET / HTTP/1.0");
+        client.print("Host: ");
+        client.println(server);
+        client.println("Accept: */*");
+        client.println("Message: Hello, from the arduino!");
+        client.print("Current RSSI: ");
+        client.println(WiFi.RSSI());
+        client.println("Connection: close");
+        client.println();
+        connected = true;
+        Serial.print("HTTP request sent to ");
+        Serial.println(server);
+        break;
+      }
+    }
+    if (!connected) return;
+  }
+}
 
-  // print the MAC address of the router you're attached to:
-  byte bssid[6];
-  WiFi.BSSID(bssid);
-  Serial.print("BSSID: ");
-  printMacAddress(bssid);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.println(rssi);
-
-  // print the encryption type:
-  byte encryption = WiFi.encryptionType();
-  Serial.print("Encryption Type:");
-  Serial.println(encryption, HEX);
-  Serial.println();
+void loop() {
+  Serial.println("Establishing first connection");
+  while (true) {
+    connect();
+    delay(3000);
+    while (status == WL_CONNECTED) {
+      delay(5000);
+      internetCheck();
+      Serial.println("Lost connection to the server.");
+    }
+    Serial.println("Lost connection to the network. Reattempting connection in 5 seconds...");
+    delay(5000);
+  }
 }
 
 void printMacAddress(byte mac[]) {
