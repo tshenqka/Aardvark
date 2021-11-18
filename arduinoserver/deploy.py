31b6dcadc48b219afa0a5032e5311c4a8bee0c82
Olivia Yixin Ouyang <y38ouyang@uwaterloo.ca>
2021-11-18 17:40:39 +0000

    Update arduinoserver/deploy.py

--- arduinoserver/deploy.py
+++ arduinoserver/deploy.py
@@ -20,11 +20,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods=["GET","POST"])
 def main():
     return "Welcome Aardvark!"
 
+
 if __name__ == "__main__":
     app.run(debug=True, host = "0.0.0.0", port=80)
+
 
 
