f4fb8ef30a284f6c428794f477943f0b1f7535d2
Olivia Yixin Ouyang <y38ouyang@uwaterloo.ca>
2021-11-18 17:00:38 +0000

    Update deploy.py

--- arduinoserver/deploy.py
+++ arduinoserver/deploy.py
@@ -16,31 +16,15 @@ class Deployment():
         pass
 
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import Flask
 
-hostName = "localhost"#
-serverPort = 8080
+app = Flask(__name__)
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        #self.wfile.write(bytes("<p>Request: %s</p >" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>Hi</p >", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+@app.route("/")
+def main():
+    return "Welcome Aardvark!"
 
 if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
+    app.run(debug=True, host = "0.0.0.0", port=80)
 
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
 
